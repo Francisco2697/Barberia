a27ead845e43be115430b6c61da03a347009345f
FrancD9 <jaziel009cr@gmail.com>
2019-06-10 19:19:33 -0500

    cpanel
    checa la ruta que no me redirecciona xd

---
+++ barberiaV1/resources/views/cpanel/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.template')
+@section('title-page') Panel @stop
+
+@section('content')
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+
+        <li class="breadcrumb-item">
+            <a href="#">Panel</a>
+
+        </li>
+
+    </ol>
+@stop
