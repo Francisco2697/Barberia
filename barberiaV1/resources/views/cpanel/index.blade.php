670c1bd3c36df345e268326a77501275fcabe07b
FrancD9 <jaziel009cr@gmail.com>
2019-06-11 19:37:57 -0500

    panel

--- barberiaV1/resources/views/cpanel/index.blade.php
+++ barberiaV1/resources/views/cpanel/index.blade.php
@@ -7,11 +7,67 @@
 @section('content')
     <!-- Breadcrumbs-->
     <ol class="breadcrumb">
-
         <li class="breadcrumb-item">
             <a href="#">Panel</a>
 
         </li>
+        <li class="breadcrumb-item">
+            <a href="#">Principal</a>
+
+        </li>
 
     </ol>
-@stop
+
+    <h1>Barberia el Spot</h1>
+    <hr>
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card-body cs-style-3">
+                <div class="grid cs-style-3" style="width: 24rem;">
+				    <figure class="figure">
+					    <img class="card-img-top" src="{{asset('images/inventario.png')}}" alt="Card image cap">
+					    <figcaption class="figure-caption">
+						    <h3>Iventario</h3>  
+						    <a class="hpv" href="">Ver</a>
+					    </figcaption>
+				    </figure>	
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card-body cs-style-3">
+                <div class="grid cs-style-3" style="width: 24rem;">
+				    <figure class="figure">
+					    <img class="card-img-top" src="{{asset('images/venta.png')}}" alt="Card image cap">
+					    <figcaption class="figure-caption">
+						    <h3>Ventas</h3>  
+						    <a class="hpv" href="">Ver</a>
+					    </figcaption>
+				    </figure>	
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card-body cs-style-3">
+                <div class="grid cs-style-3" style="width: 24rem;">
+				    <figure class="figure">
+					    <img class="card-img-top" src="{{asset('images/reporte.png')}}" alt="Card image cap">
+					    <figcaption class="figure-caption">
+						    <h3>Reportes</h3>  
+						    <a class="hpv" href="">Ver</a>
+					    </figcaption>
+				    </figure>	
+                </div>
+            </div>
+        </div>
+    </div>
+        
+@endsection
+
+@section('script')
+
+<script src="{{ asset('js/modernizr.custom.js') }}"></script>
+<script src="{{ asset('js/toucheffects.js') }}"></script>
+
+@endsection
